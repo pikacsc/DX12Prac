32e4815b1a974afe27052ceb4b6e98418465c76e
pikacsc <ccc11203@gmail.com>
2019-09-16 20:51:40 +0900

    Step_2_Draw_basic_triangle

--- stdafx.h
+++ stdafx.h
@@ -84,3 +84,15 @@ void Cleanup(); // release com objects and clean up memory
 
 void WaitForPreviousFrame(); // wait until gpu is finished with command list
 
+ID3D12PipelineState* pipelineStateObject; // pso containing a pipeline state
+
+ID3D12RootSignature* rootSignature; // root signature difines
+
+D3D12_VIEWPORT viewport; // area that output from rasterizer will be stretched to.
+
+D3D12_RECT scissorRect; // the area to draw in. pixels outside that area will not be drawn onto
+
+ID3D12Resource* vertexBuffer; // a default buffer in GPU memory that we will load vertex data for our triangle into
+
+D3D12_VERTEX_BUFFER_VIEW vertexBufferView; // a structure containing a pointer to the vertex data in gpu memory
+										   // the total size of the buffer, and the size of each element (vertex)
